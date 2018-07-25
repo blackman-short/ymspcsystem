5cc7e4a16817aed2fb8cf7a77ea1556a654966b2
blackman-short <613222346@qq.com>
2018-07-25 21:24:13 +0800

    Init pc client by vue webpack.

---
+++ yms-pc/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'yms-pc'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
