ae83aee2b737cce5593b18e7f3a4bccca1046d98
blackman-short <613222346@qq.com>
2018-07-25 22:32:37 +0800

    Adds dashboard & loading-page.

--- yms-pc/src/renderer/main.js
+++ yms-pc/src/renderer/main.js
@@ -5,10 +5,19 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+// 引入字体库
+import 'font-awesome/css/font-awesome.min.css'
+// 引入element-ui
+import ElementUI from 'element-ui'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+
 Vue.http = Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://localhost:8080'
+
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
--- yms-pc/src/renderer/router/index.js
+++ yms-pc/src/renderer/router/index.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import DashboardPage from '@/pages/dashboard'
+
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
-    {
-      path: '*',
-      redirect: '/'
-    }
-  ]
+const paths = [
+  {
+    path: '/',
+    component: DashboardPage,
+    name: '主页'
+  }
+]
+
+const routes = new Router({
+  routes: paths
 })
+
+export default routes
